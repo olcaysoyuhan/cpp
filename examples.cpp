48efe4c9624d46e8f08b8f6018fd6ee550695e8b
olcaysoyuhan <solcay50@hotmail.com>
2026-04-25 01:33:14 +0300

    Shortened if-else

--- examples.cpp
+++ examples.cpp
@@ -3,7 +3,10 @@
 int main()
 {
     int number{};
+    std::string result{};
     std::cout << "Enter a number: ";
     std::cin >> number;
     std::cout << "You entered: " << number << "\n";
+    result = (number >= 0) ? "Positive" : "Negative";
+    std::cout << "The number is: " << result << "\n";
 }
