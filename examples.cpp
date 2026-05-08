38877a28dd9cfb7d1c572d210b4833dee6e24a39
olcaysoyuhan <solcay50@hotmail.com>
2026-05-09 00:42:19 +0300

    function +

--- examples.cpp
+++ examples.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <string>
 
-void message(std::string name)
+void message(const std::string &name)
 {
-    std::cout << "Hello, " << name << "\n";
+    std::cout << "Hello, " << name << '\n';
 }
 
 int main()
 {
-    std::string name;
+    std::string name{};
     std::cout << "Enter your name: ";
     std::getline(std::cin, name);
     message(name);
