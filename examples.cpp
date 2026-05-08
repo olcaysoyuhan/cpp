31b7ce1d1ce6cebfe80c81c857073ba5bca73abc
olcaysoyuhan <solcay50@hotmail.com>
2026-05-09 00:23:43 +0300

    void function

--- examples.cpp
+++ examples.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
-#include <iomanip>
-#include <string>
 
-std::string name{"Olcay"};
-char ch{'a'};
-char ch2 = 61;
+void message()
+{
+    std::cout << "Hello World!" << "\n";
+}
 
 int main()
 {
-    std::cout << name << "\n";
-    std::cout << std::setw(10) << name << "\n";
-    std::cout << std::right << std::setw(20) << name << "\n";
-    std::cout << std::setfill('*') << std::setw(50) << name << "\n";
-    std::cout << ch << "\n";
-    std::cout << ch2 << "\n";
+    for (int i = 0; i < 5; i++)
+    {
+        message();
+    }
 }
