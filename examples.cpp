23ff1a32d1a6587c614fd205113d02e68b43c0d9
olcaysoyuhan <solcay50@hotmail.com>
2026-05-08 17:07:10 +0300

    ram example

--- examples.cpp
+++ examples.cpp
@@ -3,6 +3,8 @@
 #include <string>
 
 std::string name{"Olcay"};
+char ch{'a'};
+char ch2 = 61;
 
 int main()
 {
@@ -10,4 +12,6 @@ int main()
     std::cout << std::setw(10) << name << "\n";
     std::cout << std::right << std::setw(20) << name << "\n";
     std::cout << std::setfill('*') << std::setw(50) << name << "\n";
+    std::cout << ch << "\n";
+    std::cout << ch2 << "\n";
 }
