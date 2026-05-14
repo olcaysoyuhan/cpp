f531eda7e0fbd1bbf318d2471dcb0d705194d1af
olcaysoyuhan <solcay50@hotmail.com>
2026-05-15 00:49:23 +0300

    Prime number

--- examples.cpp
+++ examples.cpp
@@ -1,18 +1,36 @@
 #include <iostream>
 
-int sum(int x, int y)
-{
-    return x + y;
-}
-
-int get(int (*callback)(int, int))
-{
-    int result = callback(5, 4);
-    return result;
-}
-
 int main()
 {
-    int finalResult = get(sum);
-    std::cout << "Sonuç: " << finalResult << '\n';
+    unsigned short int number{};
+    bool isPrime{};
+
+    while (true)
+    {
+        std::cout << "Enter a number: ";
+        std::cin >> number;
+
+        if (number == 0)
+        {
+            break;
+        }
+
+        for (unsigned short int i = 2; i <= number; i++)
+        {
+            isPrime = true;
+            for (unsigned short int j = 2; j * j <= i; j++)
+            {
+                if (i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                std::cout << i << " ";
+            }
+        }
+        std::cout << '\n';
+    }
 }
