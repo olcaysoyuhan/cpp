7d991ef958f3a9e42f4a9faaf59c1787fe8c28a0
olcaysoyuhan <solcay50@hotmail.com>
2026-05-15 01:41:53 +0300

    Prime number function updated.

--- examples.cpp
+++ examples.cpp
@@ -1,32 +1,48 @@
 #include <iostream>
 
+bool isPrime(int j)
+{
+    if (j <= 1)
+        return false;
+
+    for (int i = 2; i <= j / i; i++)
+    {
+        if (j % i == 0)
+            return false;
+    }
+    return true;
+}
+
 int main()
 {
-    unsigned short int number{};
-    bool isPrime{};
+    int number{};
 
     while (true)
     {
-        std::cout << "Enter a number: ";
-        std::cin >> number;
+        std::cout << "Enter a number (0 to quit): ";
+
+        if (!(std::cin >> number))
+        {
+            std::cout << "Invalid input. Please enter an integer. \n";
+            std::cin.clear();
+            std::cin.ignore(10000, '\n');
+            continue;
+        }
 
         if (number == 0)
         {
+            std::cout << "Program terminated.\n";
             break;
         }
-
-        for (unsigned short int i = 2; i <= number; i++)
+        if (number < 0)
         {
-            isPrime = true;
-            for (unsigned short int j = 2; j * j <= i; j++)
-            {
-                if (i % j == 0)
-                {
-                    isPrime = false;
-                    break;
-                }
-            }
-            if (isPrime)
+            std::cout << "Please enter a non-negative integer.\n";
+            continue;
+        }
+
+        for (int i = 2; i <= number; i++)
+        {
+            if (isPrime(i))
             {
                 std::cout << i << " ";
             }
